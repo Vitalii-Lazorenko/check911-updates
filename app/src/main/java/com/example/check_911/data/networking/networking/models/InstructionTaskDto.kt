4592d8b77918b07bb2b31418235242af5b012b3d
Vitalii Lazorenko <oziylev@gmail.com>
2026-05-26 16:06:59 +0300

    Adjust instruction task DTO fields to backend response

--- app/src/main/java/com/example/check_911/data/networking/networking/models/InstructionTaskDto.kt
+++ app/src/main/java/com/example/check_911/data/networking/networking/models/InstructionTaskDto.kt
@@ -3,15 +3,16 @@ package com.example.check_911.data.networking.networking.models
 data class InstructionTaskDto(
     val id: String,
     val createdAt: String,
-    val surveyHeaderId: String,
-    val surveyQuestionId: String,
-    val surveyLogId: String,
-    val surveyQuestionText: String,
-    val surveyAnswerText: String?,
+    val instructionHeaderId: String,
+    val instructionDetailId: String,
+    val instructionLogHeaderId: String,
+    val instructionLogDetailId: String,
+    val instructionDetailTitle: String,
     val taskClaimText: String,
     val taskClaimImgUrl: String?,
     val taskAnswerText: String?,
     val taskAnswerImg: String?,
     val taskAnswerGammaId: Number?,
-    val taskHandledAt: String?
+    val taskHandledAt: String?,
+    val taskDeadlineAt: String?
 )
